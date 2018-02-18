43ea977288f37c0d39764d4852a4e86bc1326492
Nitin Gautam <andor@andor.mobi>
2018-02-18 20:25:54 +0530

    shloka reading from file issue resolved.

--- app/src/main/java/com/gita/holybooks/bhagwatgita/activity/HomePageActivity.java
+++ app/src/main/java/com/gita/holybooks/bhagwatgita/activity/HomePageActivity.java
@@ -1,5 +1,6 @@
 package com.gita.holybooks.bhagwatgita.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -28,26 +29,27 @@ public class HomePageActivity extends AppCompatActivity {
         String firstShloka = DataUtil.getShlokaTextMap().get("1_1");
         Log.d("HomePageActivity", "first shloka="+firstShloka);
 
-        TextView textView=(TextView) findViewById(R.id.editText);
-        textView.setText(firstShloka==null?"Hello World!!!":firstShloka);
+       /* TextView textView=(TextView) findViewById(R.id.editText);
+        textView.setText(firstShloka==null?"Hello World!!!":firstShloka);*/
 
         final Button nextButton = findViewById(R.id.next_button_id);
         nextButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                Intent intent = new Intent(HomePageActivity.this, HomePageActivity.class);
                 String nextShloka = DataUtil.getShlokaTextMap().get("1_4");
-                TextView textView=(TextView) findViewById(R.id.next_button_id);
+                TextView textView=(TextView) findViewById(R.id.editText);
                 textView.setText(nextShloka==null?"Hello World!!!":nextShloka);
+                startActivity(intent);
             }
         });
 
-        final Button prevButton = findViewById(R.id.prev_button_id);
+        /*final Button prevButton = findViewById(R.id.prev_button_id);
         prevButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String prevButton = DataUtil.getShlokaTextMap().get("1_2");
                 TextView textView=(TextView) findViewById(R.id.prev_button_id);
                 textView.setText(prevButton==null?"Hello World!!!":prevButton);
             }
-        });
+        });*/
     }
 }
