6765e0b9f9e197db0bc635070b437217bab8182d
Nitin Gautam <andor@andor.mobi>
2018-03-17 12:01:29 +0530

    notification service

--- app/src/main/java/com/gita/holybooks/bhagwatgita/activity/ShlokaOfTheDayActivity.java
+++ app/src/main/java/com/gita/holybooks/bhagwatgita/activity/ShlokaOfTheDayActivity.java
@@ -1,8 +1,10 @@
 package com.gita.holybooks.bhagwatgita.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,6 +12,9 @@ import com.gita.holybooks.bhagwatgita.R;
 import com.gita.holybooks.bhagwatgita.util.DataUtil;
 
 public class ShlokaOfTheDayActivity extends AppCompatActivity {
+
+    String shlokaId = null;
+    String shlokaText = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +24,32 @@ public class ShlokaOfTheDayActivity extends AppCompatActivity {
         if(extras != null)
             Log.i( "dd","Extra:" + extras.getString("shloka_id") );
 
-        Toast.makeText(getApplicationContext(),
-                "Do Something NOW" + extras.getString("shloka_id"),
+        Toast.makeText(getApplicationContext(),"Do Something NOW" + extras.getString("shloka_id"),
                 Toast.LENGTH_LONG).show();
-        String shlokaId = extras.getString("shloka_id");
+        shlokaId = extras.getString("shloka_id");
         setContentView(R.layout.shloka_of_the_day);
 
-        String shlokaText = DataUtil.shlokaTextMap.get(shlokaId);
+        //shlokaText = DataUtil.shlokaTextMap.get(shlokaId); ---? for 11_3 shlokaText was null
+
+        shlokaText = "Hello World";
 
         TextView textView=(TextView) findViewById(R.id.shlokaOfTheDayId);
         textView.setText(shlokaId);
         textView=(TextView) findViewById(R.id.shlokaOfTheDayContentId);
         textView.setText(shlokaText);
 
+
+    }
+
+    public void shareContent(View view) {
+
+        if(shlokaId!=null && shlokaText!=null){
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
+        }
 
     }
 }
